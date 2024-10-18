fbfc1f527172c59085e38e443604c49ac9339193
VIHANGA <vihanga2003nimsara@gmail.com>
2024-10-18 19:31:16 +0530

    updating

--- busbuddy/routes/api.php
+++ busbuddy/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 
 
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
